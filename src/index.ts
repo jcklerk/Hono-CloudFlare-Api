c85d43a1c5c776ff67cb3470da0ba315ed3e16d6
Justin Klerk <justin@klerk.nl>
2026-07-04 18:18:23 +0200

    build in function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { createHonoApp } from "@/lib/hono";
 import routeAuth from "@/routes/auth";
 import routeV1 from "@/routes/v1";
+import { inspectRoutes } from "hono/dev";
 import { logger } from "hono/logger";
 import { prettyJSON } from "hono/pretty-json";
 
@@ -15,19 +16,17 @@ app.route("/v1", routeV1);
 app.use("/", prettyJSON({ force: true }));
 
 app.get("/", (c) => {
-	const routes = app.routes.map((route) => {
-		console.log(
-			`Route: ${route.method} ${route.path} - Handler: ${route.handler.name}`,
-		);
-		if (!route.handler.name)
-			return {
-				method: route.method,
-				path: route.path,
-			};
-	});
-
+	const routes = inspectRoutes(app)
+		.filter((route) => route.isMiddleware === false)
+		.map((route) => ({
+			method: route.method,
+			path: route.path,
+		}));
 	return c.json(
-		{ message: "Welcome to the API", routes: routes.filter(Boolean) },
+		{
+			message: "Welcome to the API",
+			routes: routes,
+		},
 		200,
 	);
 });
